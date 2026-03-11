5177bad3f6fc7194f88da25d68ee686435e31e4a
Kevin <kvillalobos917@gmail.com>
2026-03-11 15:52:12 -0500

    Cmnu

--- src/CrearMenu.py
+++
@@ -1,7 +0,0 @@
-def menu():
-
-    print("---MENU PRINCIPAL---\n")
-    print("1. Registar producto")
-    print("2. Visualizar inventario")
-    print("3. Calcular estadisticas")
-    print("4. Salir")
