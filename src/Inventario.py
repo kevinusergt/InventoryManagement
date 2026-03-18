618cc380dcfe428b990ab5d02689db9d099e6a23
spideychill777 <spideychill2711@gmail.com>
2026-03-18 15:01:56 -0500

    CalcularEst

--- src/Inventario.py
+++ src/Inventario.py
@@ -3,7 +3,7 @@ import time
 
 inventario = []
 
-def registrar_productos(Nombre, Precio, Cantidad):
+def registrar_productos(nombre, precio, cantidad):
   
   producto = {
    'Nombre': nombre,
@@ -24,6 +24,21 @@ def mostrar_inventario():
       print(f"Producto: {i['Nombre']} | Precio: {i['Precio']} $ | Cantidad: {i['Cantidad']} unidades \n")
     time.sleep(1)
 
+def calcular_inventario():
+  if len(inventario) == 0:
+    print('inventario vacio')
+  else:
+    
+    stock = len(inventario)
+    
+    
+    calcular = sum(i['Precio'] * i['Cantidad'] for i in inventario)
+    print('---REPORTE DE INVENTARIO---')
+    print(f'Total de productos: {stock}')
+    print(f'El valor total del inventario: {calcular} $')
+      
+    
+
 def actualizar_inventrio():
   if len (inventario) == 0:
     print('El inventario está vacio!')        
@@ -33,9 +48,10 @@ def menu():
  print("||Menu Inventario|| \n")
  print("1. Registrar productos")
  print("2. Mostar inventario")
- print("3. Actualizar inventario")
- print("4. Eliminar producto")
- print("5. salir \n")
+ print('3. Calcular estadisticas')
+ print("4. Actualizar inventario")
+ print("5. Eliminar producto")
+ print("6. salir \n")
 
 
 
@@ -43,19 +59,25 @@ def menu():
 
 while True:
  menu()
- try:
-  option = int(input("Choose an opcion: \n"))
+ while True:
+  try:
+    option = int(input("Choose an opcion: \n"))
+    break
 
- except ValueError:
-  print("Enter a valid opcion!")
-  time.sleep(1)
+  except ValueError:
+    print("Enter a valid opcion! \n")
+    time.sleep(1)
  
 
  if option == 1:
-  nombre = input("Introduce el nombre del producto:\n ")
+  nombre = input("Introduce el nombre del producto: \n")
   while True:
     try:
-      precio = float(input("Ingrese el precio: \n "))
+      precio = float(input("Ingrese el precio: \n"))
+      if precio < 0:
+        print('Ingrese un precio real!')
+        continue
+
       break
     except ValueError:
       print('Por favor ingresa un precio con decimales!')
@@ -63,6 +85,9 @@ while True:
   while True:  
     try:
       cantidad = int(input("Ingrese la cantidad: \n"))
+      if cantidad < 0:
+        print('Introduzca un numero entero positivo!')
+        continue
       break  
     except ValueError:
      print('Ingresa un numero entero!')  
@@ -72,12 +97,21 @@ while True:
    mostrar_inventario()
 
  elif option == 3:
-   print('Aun en proceso')
+   calcular_inventario()
+   print()
+   time.sleep(1)
 
  elif option == 4:
-   print('Aun en proceso') 
-
+   print('Aun en proceso \n') 
+ 
  elif option == 5:
+   print('Aun en proceso \n') 
+ 
+ 
+ elif option == 6:
    time.sleep(1)
    break
-       
+  
+ else:
+    print('Ingrese una opcion del menú \n')
+    time.sleep(1)     
